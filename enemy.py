13249df26b068d6ab88100329c80d28d89fb6d88
Cristian Victor Ionescu <57479296+cristid6@users.noreply.github.com>
2022-09-14 19:16:00 +0300

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,24 +1,25 @@
 class Enemy:
     def __init__(self, type_of):
-        self.type = type_of
+        self.type_of = type_of
+        
 
     def attack(self):
         pass
 
 class Goblin(Enemy):
     def __init__(self):
-        super().__init__(self)
+        super().__init__(self, 'Goblin')
         self.life = 50
         self.attack_power = 50
 
 class Orc(Enemy):
     def __init__(self):
-        super().__init__(self)
+        super().__init__(self, 'Orc')
         self.life = 70
         self.attack_power = 70
 
 class Troll(Enemy):
     def __init__(self):
-        super().__init__(self)
+        super().__init__(self, 'Troll')
         self.life = 100
         self.attack_power = 100
